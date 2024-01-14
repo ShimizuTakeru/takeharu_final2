a676d6cd69d731b59bfa4b77764419c4e9dfd8e4
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2024-01-14 21:07:38 +0900

    Update kaitou1-1.js

--- kaitou1-1.js
+++ kaitou1-1.js
@@ -1,4 +1,6 @@
-function kaito01(){
-  if(document.getElementById('answer').value=='夢'){
-    alert('あたり！');}
+function kotae01(){
+  if(document.getElementById('kaitou01').value=='正しい'){
+    alert('正解は「誤り」です');}
+  else(document.getElementById('kaitou01').value=='誤り'){
+    alert('正解です！');}
 }
