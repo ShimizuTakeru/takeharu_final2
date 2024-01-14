1e64f9d406b9bd60e8e34b5fd49fe553f5eda914
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2024-01-14 21:19:07 +0900

    Update kaitou1-1.js

--- kaitou1-1.js
+++ kaitou1-1.js
@@ -1,6 +1,8 @@
 function kotae01(){
   if(document.getElementById('kaito01').value=='正しい'){
     alert('正解は「誤り」です');}
-  else(document.getElementById('kaito01').value=='誤り'){
-    alert('正解です！');}
+    else if(document.getElementById('kaito01').value=='誤り'){
+      alert('正解です！');}
+  else{
+    alert('不正解・・・');}
 }
