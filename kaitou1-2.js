a672ef063ef300ce6fa0bebef06e852ee21e684b
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2024-01-29 16:29:19 +0900

    Update kaitou1-2.js

--- kaitou1-2.js
+++ kaitou1-2.js
@@ -7,16 +7,12 @@
     alert('不正解・・・');}
 }*/
 
-/*function sayhello68(){alert('Hello,'+document.getElementById('who68').innerHTML+'!');}
-function taro68(){document.getElementById('who68').innerHTML='太郎';}
-function hanako68(){document.getElementById('who68').innerHTML='花子';}*/
-
 function sentaku01(){document.getElementById('sentakushi').innerHTML='瀞峡・瀞八丁';}
 function sentaku02(){document.getElementById('sentakushi').innerHTML='狭山湖';}
 function sentaku03(){document.getElementById('sentakushi').innerHTML='長瀞';}
 function sentaku04(){document.getElementById('sentakushi').innerHTML='行田';}
 function kaitou02(){
-  if(document.getElementById('sentakushi').innerHTML==sentku03()){
+  if(document.getElementById('sentakushi').innerHTML=='sentku03();'){
     alert('正解！');}
   else{
     alert('不正解・・・');}
