fcdfc3221da04612082f988c14c2f519a00b77c9
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2024-01-29 21:33:01 +0900

    Update kaitou1-2.js

--- kaitou1-2.js
+++ kaitou1-2.js
@@ -1,11 +1,4 @@
-/*function kotae02(){
-  if(document.getElementById('answer1').value=='正しい'){
-    alert('正解は「誤り」です');}
-    else if(document.getElementById('answer1').value=='誤り'){
-      alert('正解です！');}
-  else{
-    alert('不正解・・・');}
-}*/
-
 function sentaku1(){alert('不正解・・・');}
 function sentaku2(){alert('正解！');}
+
+function kaisetu1()
