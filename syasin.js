0c661aeb2316b870415950b4dea798a8c405e847
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2024-01-30 11:31:15 +0900

    Update syasin.js

--- syasin.js
+++ syasin.js
@@ -1,1 +1,5 @@
 function imgchange1(){document.getElementById('gazo').src='./image/写真1.jpg';}
+function imgchange2(){document.getElementById('gazo').src='./image/写真5.jpg';}
+function imgchange3(){document.getElementById('gazo').src='./image/写真6.jpg';}
+function imgchange4(){document.getElementById('gazo').src='./image/写真3.jpg';}
+function imgchange5(){document.getElementById('gazo').src='./image/写真4.jpg';}
