ff684f6d4a35cc339078fcd90cb032adb5124021
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2024-01-30 11:31:47 +0900

    Update syasin.js

--- syasin.js
+++ syasin.js
@@ -1,3 +1,4 @@
+/*index.html　画像を切り替える*/
 function imgchange1(){document.getElementById('gazo').src='./image/写真1.jpg';}
 function imgchange2(){document.getElementById('gazo').src='./image/写真5.jpg';}
 function imgchange3(){document.getElementById('gazo').src='./image/写真6.jpg';}
